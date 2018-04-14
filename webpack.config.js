1a4a31b4bea786ab2865635fdd00df4043a27de7
zzuyoung <zzyouyang@qq.com>
2018-04-15 02:55:41 +0800

    4-1 webpack配置css单独分离打包

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HTMLPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
+const ExtractPlugin = require('extract-text-webpack-plugin')
 
 
 const isDev = process.env.NODE_ENV === 'development'
@@ -9,7 +10,7 @@ const config = {
 	target: 'web', // 指定是web平台
 	entry: path.join(__dirname, 'src/index.js'),	// 入口文件
 	output: {
-		filename: 'bundle.js',
+		filename: 'bundle.[hash:8].js',
 		path: path.join(__dirname, 'dist')
 	},
 	module: {
@@ -21,27 +22,6 @@ const config = {
 			{
 				test: /\.jsx$/,
 				loader: 'babel-loader'
-			},
-			{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					'css-loader'
-				]
-			},
-			{
-				test: /\.styl$/,
-				use: [
-					'style-loader',
-					'css-loader',
-					{
-						loader: 'postcss-loader',
-						options: {
-							sourceMap: true,
-						}
-					},
-					'stylus-loader'
-				]
 			},
 			{
 				test: /\.(gif|jpg|jpeg|png|svg)$/,
@@ -70,6 +50,20 @@ const config = {
 
 // 根据环境设置
 if (isDev) {
+	config.module.rules.push({
+		test: /\.styl$/,
+		use: [
+			'style-loader',
+			'css-loader',
+			{
+				loader: 'postcss-loader',
+				options: {
+					sourceMap: true,
+				}
+			},
+			'stylus-loader'
+		]
+	},)
 	config.devtool = '#cheap-module-eval-source-map'
 	config.devServer = {
 		port: 8000,
@@ -85,7 +79,31 @@ if (isDev) {
 	}
 	config.plugins.push(
 		new webpack.HotModuleReplacementPlugin(),
-		new webpack.NoEmitOnErrorsPlugin()
+		new webpack.NoEmitOnErrorsPlugin(),
+		new ExtractPlugin()
+	)
+} else {
+	config.output.filename = '[name].[chunkhash:8].js'
+	config.module.rules.push(
+			{
+			test: /\.styl$/,
+			use: ExtractPlugin.extract({
+				fallback: 'style-loader',
+				use: [
+					'css-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							sourceMap: true,
+						}
+					},
+					'stylus-loader'
+				]
+			})
+		}
+	)
+	config.plugins.push(
+		new ExtractPlugin('styles.[contentHash:8].css')
 	)
 }
 
